a40446ae07511e0e38a0119e31d8fabef3d9d061
William Mello <mello.william01@gmail.com>
2021-10-03 15:07:43 -0300

    UpFiles
    UpFiles

---
+++ src/main/java/one/digitalinnovation/beerstock/BeerstockApplication.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.beerstock;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class BeerstockApplication {
+
+	public static void main(String[] args) {
+		run(BeerstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/beerstock/demo/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
